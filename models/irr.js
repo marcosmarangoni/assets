2fe7da1e1aebf16616f2a089780094b610f11435
Oliveira Reboucas <oliveirareboucasc@student.douglascollege.ca>
2018-11-08 16:27:08 -0800

    Better IRR

--- models/irr.js
+++ models/irr.js
@@ -1,41 +1,27 @@
 /** irr.js **/
 
     function calc(ativo) {
-        ativo.trades.push({ date : "2018-11-06", tipo: "p" , value: Number((ativo.saldo * ativo.unitario).toFixed(2))});
-        //console.log(ativo);
-        return irr(ativo.trades);
+        var today = new Date();
+        ativo.trades.push(
+            {date : today.getFullYear()+"-"+(1+today.getMonth())+("-0"+today.getDate()).slice(-3),
+             tipo: "p" , value: Number((ativo.saldo * ativo.unitario).toFixed(2))
+        });
+        
+        if(ativo.trades.length>2) {
+            return irr(ativo.trades);
+        } else {
+            return simpleReturn(ativo.trades);
+        }
     }
 
     function irr(fluxo) {
         var sum_in = 0;
         var sum_out = 0;
-        var date_start = 0;
-        var daysonyear_start;
-        var interval_total = 0;
+
+        fluxo = injectInterval(fluxo);
 
         for (i = 0; i < fluxo.length; ++i) {
-            date_splited = fluxo[i].date.split('-');
-            dia = new Date(date_splited[0], (date_splited[1]-1), date_splited[2], 0,0,0,0); // YYYY-MM-DD
-            if(date_start==0) { // Primeiro trade
-                date_start = dia;
-                fluxo[i].interval = 0;
-                //console.log(new Date(Number(date_splited[0])+1, 0, 1, 0,0,0,0));
-                daystoyear_start = Math.ceil(((new Date(Number(date_splited[0])+1, 0, 1, 0,0,0,0)) - dia) /1000 / 3600 / 24);
-            } else {
-                year_start = date_start.getFullYear();
-                year_date = dia.getFullYear();
-                daysonyear = Math.ceil((dia - (new Date(date_splited[0], 0, 1, 0,0,0,0)))/1000 / 3600 / 24);
-                interval = year_date - year_start + (daysonyear/365)  + (daystoyear_start/365) - 1 ;
-                //console.log('daysonyear: '+daysonyear+' daysonyear_start: '+daystoyear_start)
-                fluxo[i].interval = interval;
-                interval_total += Math.abs(interval); //Protect Mesmo DIa
-            }
             if(fluxo[i].value > 0) {sum_in += fluxo[i].value;} else {sum_out -= fluxo[i].value;}
-            //console.log(fluxo[i]);
-        }
-
-        if(interval_total < 0.013) {  //Protect Mesmo DIa
-            fluxo[fluxo.length-1].interval = 0.013;
         }
         
         var guess = (sum_in/sum_out);
@@ -53,7 +39,6 @@
                 vp_all += fluxo[x].value / guess ** fluxo[x].interval;
             }
             //console.log('Try: '+i+' VP: '+vp_all);
-
             vp_third = vp_second;
             guess_third = guess_second;
             vp_second = vp_all;
@@ -71,6 +56,41 @@
         while (Math.abs(vp_all) > 0.01 && guess<=100 && i <= 500);
         console.log('IRR: '+Number((guess-1)*100).toFixed(2)+' Try:'+i );
         return Number(((guess-1)*100).toFixed(2));
+    }
+
+    function simpleReturn(fluxo) {
+        fluxo = injectInterval(fluxo);
+        var taxa;
+        taxa = Math.pow(fluxo[1].value/-fluxo[0].value,(1/Math.abs(fluxo[1].interval)))-1;
+        //console.log(fluxo[1].value/-fluxo[0].value);
+        //console.log(fluxo[1].interval);
+        return (taxa*100).toFixed(2);
+    }
+
+    function injectInterval(fluxo) {
+        var date_start = 0;
+        var interval_total = 0;
+        
+        for (i = 0; i < fluxo.length; ++i) {
+            date_splited = fluxo[i].date.split('-');
+            dia = new Date(date_splited[0], (date_splited[1]-1), date_splited[2], 0,0,0,0); // YYYY-MM-DD
+            if(date_start==0) { // Primeiro trade
+                date_start = dia;
+                fluxo[i].interval = 0;
+                daystoyear_start = Math.ceil(((new Date(Number(date_splited[0])+1, 0, 1, 0,0,0,0)) - dia) /1000 / 3600 / 24);
+            } else {
+                year_start = date_start.getFullYear();
+                year_date = dia.getFullYear();
+                daysonyear = Math.ceil((dia - (new Date(date_splited[0], 0, 1, 0,0,0,0)))/1000 / 3600 / 24);
+                interval = year_date - year_start + (daysonyear/365)  + (daystoyear_start/365) - 1 ;
+                fluxo[i].interval = interval;
+                interval_total += Math.abs(interval); //Protect Mesmo DIa
+            }
+        }
+        if(interval_total < 0.013) {  //Protect Mesmo DIa
+            fluxo[fluxo.length-1].interval = 0.013;
+        }
+        return fluxo;
     }
 
 
