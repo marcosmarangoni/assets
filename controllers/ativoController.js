643719dcbbea7d36051d9f3b8ecc29ac10570a05
cesarreboucas <cesar.reboucas@gmail.com>
2018-11-10 20:03:21 -0800

    AtivoControler modified

--- controllers/ativoController.js
+++ controllers/ativoController.js
@@ -1,30 +1,33 @@
 let IRR = require('../resources/irr');
 const Ativo = require('../models/ativo');
 let mongoose = require('mongoose');
+const async = require('async');
 
 // Temporary:
 const User = require('../models/user');
 //let user;
-
+//let user;
+/*
 const user = async () => {
     return await User.find({username: "cesar.reboucas@gmail.com"})
 };
 user().then(users => {
     //console.log(users);
 })
+*/
 
-
-/*async function getUser(username) {
+async function getUser(username) {
     
-    let user = await User.find({username: username});
+    //let user = await User.find({username: username});
+    let user = await User.find();
     //console.log(user);
     return user;
     //response.return(user);
-}*/
+}
 //getUser("aaa");
 //console.log(getUser("cesar.reboucas@gmail.com"));
 //getUser("cesar.reboucas@gmail.com").then(a);
-console.log(user());
+
 //then(console.log);
 //Fake User_ID:
 //const user = { _id : "5be5d8d44c07401ce455de9d"};
@@ -50,6 +53,12 @@ async function indexList(request, response) {
     for(let x = 0; x < Ativos.length ; ++x) {
         Ativos[x].set('retorno', IRR.calc(Ativos[x]), { strict: false });
     }
+
+    let Users = await User.find();
+    console.log(Ativos[0]);
+    
+
+
     response.send(Ativos);
 }
 
@@ -105,14 +114,7 @@ async function createAtivo(request, response) {
  */
 async function createTrade(request, response) {    
 
-    /*(async () => {
-        try {
-          let ativo = await Ativo.find({_id:request.body._id});
-          console.log('num', ativo);
-        } catch(e) {
-          console.log('Error caught');
-        }
-      })();*/
+    
 
     const id = request.body.id;
     const novotrade = {
@@ -158,20 +160,6 @@ async function editTrade(request, response) {
  */
 async function editAtivo(request, response) {
 
-    /*const ativoParams = {
-        user_id: mongoose.Types.ObjectId(user.id) ,
-        codigo: request.body.codigo,
-        saldo: request.body.quantidade,
-        unitario: unit,
-        div_projection: {timesperyear:0, value:0, datestart:null, dateend: null},
-        trades: {
-            trade_id: mongoose.Types.ObjectId(),
-            date: request.body.date,
-            tipo: request.body.tipo,
-            value: Number(request.body.valor),
-            comment: "Inicial",
-        },
-    }*/
 
     ativo = await Ativo.findOneAndUpdate({_id: request.body.id, user_id:user._id}, { 
         $set: {
@@ -184,6 +172,27 @@ async function editAtivo(request, response) {
     console.log(JSON.stringify({_id: request.body.id, user_id:user._id}));
     response.redirect('/ativos');
 }
+
+async.waterfall([
+    function(cb) {
+        console.log("Exec I");
+        User.find({},cb);
+        
+    },
+    function(cb2) {
+        //setTimeout(function(){ console.log("Espera_Ended"); }, 3000,cb2);
+        console.log("Exec II",cb2);
+    },
+    function(cb3) {
+        console.log("Exec III",cb3);
+    }   
+],  function (results) {
+        console.log(JSON.stringify(results));
+});
+
+
+
+
 
 module.exports = {
     /*
